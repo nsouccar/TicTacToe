5412cb4f75d8921dfab912389d0ddb8372bd71f2
Noa <noasouccar@icloud.com>
2025-09-22 12:39:07 -0400

    basic layout

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,24 @@
+
+
+
+function App() {
+
+  return (
+    <>
+
+    <div className = "grid-container">
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 1</div>
+      <div className="grid-item">Item 1</div>
+
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
